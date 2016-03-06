79d72b54e5efc03f1bb24cb415e3622b9e4aa164
Sean Oldham <seanosaur@me.com>
2016-03-05 19:22:59 -0600

    fix results grid, more styling adjs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,10 +9,18 @@ class HomeController < ApplicationController
     Tmdb::Api.key(ENV['TMDB_KEY'])
     search = params[:search]
     @type = params[:media_type]
-    if @type == "movie"
-      @results = Tmdb::Movie.find(search)
-    elsif @type == "tv"
-      @results = Tmdb::TV.find(search)
+    if search != ""
+      if @type == "movie"
+        @results = Tmdb::Movie.find(search)
+      elsif @type == "tv"
+        @results = Tmdb::TV.find(search)
+      else
+        flash[:alert] = "You must choose Movie or TV."
+        redirect_to root_path
+      end
+    else
+      flash[:alert] = "You must enter a search term."
+      redirect_to root_path
     end
   end
 end
