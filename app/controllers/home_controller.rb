8872861c3050ae9b0288e7f03cb68a0bb4d11ccc
Sean Oldham <seanosaur@me.com>
2016-05-28 12:30:23 -0500

    populate current list and allow item to move

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @board = Trello::Board.find(ENV['BOARD_ID'])
     @movie_list = Trello::List.find(ENV['TO_WATCH_LIST_ID'])
     @movie_cards = @movie_list.cards
   end
@@ -26,17 +25,38 @@ class HomeController < ApplicationController
   end
 
   def trello_add
-    if params[:button_name] == "currently_watching"
-      Trello::Card.create(name: params[:result_title], list_id: ENV['WATCHING_LIST_ID'])
+    if params[:button_name] == "watching"
+      Trello::Card.create(name: params[:result_title], list_id: ENV['WATCHING_LIST_ID'], desc: "#{params[:result_id]}\n#{params[:poster_path]}")
       flash[:alert] = "#{params[:result_title]} added to Currently Watching!"
       redirect_to root_path
     elsif params[:button_name] == "to_watch"
-      Trello::Card.create(name: params[:result_title], list_id: ENV['TO_WATCH_LIST_ID'])
+      Trello::Card.create(name: params[:result_title], list_id: ENV['TO_WATCH_LIST_ID'], desc: "#{params[:result_id]}\n#{params[:poster_path]}")
       flash[:alert] = "#{params[:result_title]} added to Watch Later!"
       redirect_to root_path
     elsif params[:button_name] == "watched"
-      Trello::Card.create(name: params[:result_title], list_id: ENV['WATCHED_LIST_ID'])
+      Trello::Card.create(name: params[:result_title], list_id: ENV['WATCHED_LIST_ID'], desc: "#{params[:result_id]}\n#{params[:poster_path]}")
       flash[:alert] = "#{params[:result_title]} added to Already Watched!"
+      redirect_to root_path
+    else
+      flash[:alert] = "#{params[:result_title]} could not be added, please try again and tell Sean you broke his app."
+    end
+  end
+
+  def trello_move
+    if params[:button_name] == "watching"
+      current_card = Trello::Card.find(params[:id])
+      current_card.move_to_list(ENV['WATCHING_LIST_ID'])
+      flash[:alert] = "#{params[:result_title]} moved to Currently Watching!"
+      redirect_to root_path
+    elsif params[:button_name] == "to_watch"
+      current_card = Trello::Card.find(params[:id])
+      current_card.move_to_list(ENV['TO_WATCH_LIST_ID'])
+      flash[:alert] = "#{params[:result_title]} moved to Watch Later!"
+      redirect_to root_path
+    elsif params[:button_name] == "watched"
+      current_card = Trello::Card.find(params[:id])
+      current_card.move_to_list(ENV['WATCHED_LIST_ID'])
+      flash[:alert] = "#{params[:result_title]} moved to Already Watched!"
       redirect_to root_path
     else
       flash[:alert] = "#{params[:result_title]} could not be added, please try again and tell Sean you broke his app."
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get '/search', to: redirect('/')
   post '/trello_add', to: 'home#trello_add'
   get '/trello_add', to: redirect('/')
+  post '/trello_move', to: 'home#trello_move'
+  get '/trello_move', to: redirect('/')
 end
