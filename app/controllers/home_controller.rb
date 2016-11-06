536a5ba7ab35474453e9fa3aaa66686ff54dd958
Sean Oldham <seanosaur@me.com>
2016-11-06 11:39:19 -0600

    double quotes to single quotes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,31 +9,31 @@ class HomeController < ApplicationController
     search = params[:search]
     @search_term = search
     @type = params[:media_type]
-    if search != ""
-      if @type == "movie"
+    if search != ''
+      if @type == 'movie'
         @results = Tmdb::Movie.find(search)
-      elsif @type == "tv"
+      elsif @type == 'tv'
         @results = Tmdb::TV.find(search)
       else
-        flash[:alert] = "You must choose a media type."
+        flash[:alert] = 'You must choose a media type.'
         redirect_to root_path
       end
     else
-      flash[:alert] = "You must enter a search term."
+      flash[:alert] = 'You must enter a search term.'
       redirect_to root_path
     end
   end
 
   def trello_add
-    if params[:button_name] == "watching"
+    if params[:button_name] == 'watching'
       Trello::Card.create(name: params[:result_title], list_id: ENV['WATCHING_LIST_ID'], desc: "#{params[:result_id]}\n#{params[:poster_path]}")
       flash[:alert] = "#{params[:result_title]} added to Currently Watching!"
       redirect_to root_path
-    elsif params[:button_name] == "to_watch"
+    elsif params[:button_name] == 'to_watch'
       Trello::Card.create(name: params[:result_title], list_id: ENV['TO_WATCH_LIST_ID'], desc: "#{params[:result_id]}\n#{params[:poster_path]}")
       flash[:alert] = "#{params[:result_title]} added to Watch Later!"
       redirect_to root_path
-    elsif params[:button_name] == "watched"
+    elsif params[:button_name] == 'watched'
       Trello::Card.create(name: params[:result_title], list_id: ENV['WATCHED_LIST_ID'], desc: "#{params[:result_id]}\n#{params[:poster_path]}")
       flash[:alert] = "#{params[:result_title]} added to Already Watched!"
       redirect_to root_path
@@ -43,17 +43,17 @@ class HomeController < ApplicationController
   end
 
   def trello_move
-    if params[:button_name] == "watching"
+    if params[:button_name] == 'watching'
       current_card = Trello::Card.find(params[:id])
       current_card.move_to_list(ENV['WATCHING_LIST_ID'])
       flash[:alert] = "#{params[:result_title]} moved to Currently Watching!"
       redirect_to root_path
-    elsif params[:button_name] == "to_watch"
+    elsif params[:button_name] == 'to_watch'
       current_card = Trello::Card.find(params[:id])
       current_card.move_to_list(ENV['TO_WATCH_LIST_ID'])
       flash[:alert] = "#{params[:result_title]} moved to Watch Later!"
       redirect_to root_path
-    elsif params[:button_name] == "watched"
+    elsif params[:button_name] == 'watched'
       current_card = Trello::Card.find(params[:id])
       current_card.move_to_list(ENV['WATCHED_LIST_ID'])
       flash[:alert] = "#{params[:result_title]} moved to Already Watched!"
