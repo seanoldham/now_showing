e180073500bdfb7e011a631373d1ce1fab9a52c5
Sean Oldham <seanosaur@me.com>
2016-11-11 14:23:39 -0600

    add scopes for watched/unwatched

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ActiveRecord::Base
   validates_uniqueness_of :uniqueid
   default_scope { order('name') }
+  scope :to_watch, -> { where(status: 0 ) }
+  scope :watched, -> { where(status: 1 ) }
 end
